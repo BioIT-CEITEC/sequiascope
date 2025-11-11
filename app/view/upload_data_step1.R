9b87b93611f6e92441271341f19deb002f245118
Kateřina Jurásková <245829@muni.cz>
2025-11-11 09:36:13 +0100

    documentation update

--- app/view/upload_data_step1.R
+++ app/view/upload_data_step1.R
@@ -85,9 +85,9 @@ step1_server <- function(id, path, patients, datasets, tumor_pattern, normal_pat
     next1_btn <- reactiveVal(NULL)
     load_click <- reactiveVal(NULL)
     
-    # Nastavení pouze složky input_files jako root
-    input_files_path <- file.path(getwd(), "input_files")
-    wd <- c(input_files = input_files_path)
+    # wd <- c(home = getwd())
+    
+    wd <- c(home = getwd(), input_files = "/input_files")
     shinyDirChoose(input, "dir", roots = wd)
     
     observeEvent(input$dir, {
@@ -245,7 +245,7 @@ step1_server <- function(id, path, patients, datasets, tumor_pattern, normal_pat
     }
     
     return(list(
-      next1              = reactive(next1_btn(1)),
+      next1              = reactive(next1_btn()),
       load_request       = reactive(load_click()),
       restore_ui_inputs  = restore_ui_inputs
     ))
